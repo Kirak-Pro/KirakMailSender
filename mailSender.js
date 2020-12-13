ed072c3209ab173da8fdcef2327a1cd90383ccf7
Kirak-Pro <71726405+Kirak-Pro@users.noreply.github.com>
2020-12-13 20:49:49 +0100

    Add files via upload

---
+++ mailSender.js
@@ -0,0 +1,26 @@
+module.exports.Init = () => {
+    alt.mailer = {};
+    alt.mailer.sendMail = (to, subject, message) => {
+        var nodemailer = require("nodemailer");
+        var transporter = nodemailer.createTransport({
+            host: "mail.example.com",
+            port: 587, //SMTP Port
+            secure: true, // true for 465, false for other ports
+            auth: {
+              user: 'username', // generated ethereal user
+              pass: 'password' // generated ethereal password
+            }
+        });
+        message += "<br /><br /> Cordialement, Crew's Life RP Team.";
+        const mailOptions = {
+            from: 'YOUR ADRESSE MAIL',
+            to: to,
+            subject: subject,
+            html: message
+        };
+        transporter.sendMail(mailOptions, function(err, info) {
+            if (err) alt.log(err)
+            else alt.log(info);
+        });
+    }
+}
